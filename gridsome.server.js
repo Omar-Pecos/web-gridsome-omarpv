476fd05cd327683ad73d16fda39f408643965684
omarpecos <omarpecos@gmail.com>
2020-11-25 00:00:52 +0100

    get API data

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,13 +1,78 @@
-// Server API makes it possible to hook into various parts of Gridsome
-// on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
+const axios = require('axios');
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const baseURL = 'https://api-node-portfolio.omarpv.repl.co';
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+  api.loadSource(async ({ addCollection }) => {
+
+     const techsCollection = addCollection('Tech');
+
+     /* get Techs */
+      var {data} = await axios.get( baseURL + '/api/techs');
+      const techData = data.data;
+
+      for ( const tech of techData ){
+        techsCollection.addNode({
+            id : tech._id,
+            name : tech.name,
+            type : tech.type,
+            icon : tech.icon
+        })
+      }
+
+      const profileCollection = addCollection('Profile');
+
+      /* get Profile */
+      var {data} = await axios.get( baseURL + '/api/profile');
+      const profileData = data.data;
+
+      profileCollection.addNode({
+            id : profileData._id,
+            intro : profileData.intro,
+            text : profileData.text,
+            about : profileData.about,
+            version : profileData.version
+        })
+    
+      const coursesCollection = addCollection('Course');
+
+      /* get Courses */
+        var {data} = await axios.get( baseURL + '/api/courses');
+        const courseData = data.data;
+  
+        for ( const course of courseData ){
+          coursesCollection.addNode({
+              id : course._id,
+              name : course.name,
+              description : course.description,
+              platform : course.platform,
+              duration : course.duration,
+              language : course.language,
+              url : course.url,
+              techs : course.techs,
+              done : course.done,
+          })
+        }
+
+        const projectsCollection = addCollection('Project');
+
+        /* get Courses */
+          var {data} = await axios.get( baseURL + '/api/projects');
+          const projectData = data.data;
+    
+          for ( const project of projectData ){
+            projectsCollection.addNode({
+                id : project._id,
+                name : project.name,
+                description : project.description,
+                type : project.type,
+                images : project.images,
+                files : project.files,
+                url : project.url,
+                techs : project.techs,
+                pinned : project.pinned,
+            })
+          }
   })
 
   api.createPages(({ createPage }) => {
